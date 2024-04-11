84b32d073dd0a1fb43f694f106d749477a0232cf
Gopal Gupta <guptaygo@amazon.com>
2024-04-11 12:55:37 +0530

    Initial commit

---
+++ backend/sociofy/src/main/java/com/myorg/sociofy/SociofyApplication.java
@@ -0,0 +1,13 @@
+package com.myorg.sociofy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SociofyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SociofyApplication.class, args);
+  }
+
+}
---
+++ backend/sociofy/src/test/java/com/myorg/sociofy/SociofyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myorg.sociofy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SociofyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
